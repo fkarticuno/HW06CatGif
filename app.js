77765b12920d69e9720539529662d01d71691792
FREDERICK ARTICUNO <mckiyoshi@gmail.com>
2019-09-13 22:56:45 -0400

    Button only works in HTML script, API call is broken

---
+++ app.js
@@ -0,0 +1,55 @@
+
+userInput = 'cat';  
+
+// Build content area
+
+function buildMain() {
+    var mainrow = $('<div>');
+    mainrow.attr('class','col-md-8');
+    mainrow.attr('id','gifsgohere');
+    var searchpoint = $('<div>');
+    searchpoint.attr('class','col-md-4');
+    var searchinput = '<input type="text" id="ipt"></input><input type="button" id="uSubmit"></input>';
+    searchpoint.html(searchinput);;
+    $('#content').append(mainrow, searchpoint);
+}
+
+// Build storage for gifs
+var numofgifs = 0;
+var userinput = 0;
+function buildSub() {
+    console.log('button click moved to buildsub()');
+    userinput = $('#ipt').val();
+    $('#gifsgohere').prepend(callGiphy(userinput));
+    numofgifs++;
+};
+
+// Submit gifreq
+ $('#uSubmit').click(function() {
+    console.log('button clicked')
+    buildSub();
+    })
+
+
+
+// GIPHY integration
+function callGiphy(gifreq) {
+    console.log('callgiphy() '+userinput)
+    var APIKey = "OrGsAQPACjDG7CuKA31b1bSM9ZqDShC3";
+    var queryURL = "http://api.giphy.com/v1/gifs/search?random&tag="+userinput+"&api_key="+APIKey;
+    $.ajax({
+    url: queryURL,
+    method: "GET"
+    }).then(function(response) {
+    console.log(response);
+    console.log(response.data[0].embed_url);
+    var ima = response.data[0].images.fixed_height.url;
+    var builtimage = '<img src='+ima+'/>';
+    return builtimage;
+    });
+}
+
+
+// Initialize
+
+buildMain();
